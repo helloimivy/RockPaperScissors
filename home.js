ef1b165ac19a882f28e2a7597606da908a58490e
Ivy Gross <rodney.grossiv@gmail.com>
2026-03-13 02:51:30 -0700

    Reworked game logic to work on webpage. Added buttons for gameplay. DOM practice.

--- home.js
+++ home.js
@@ -1,4 +1,53 @@
 "use strict";
+// practicing using DOM; usually would create more inside of HTML document
+const container = document.getElementById("container");
+
+const humanRockSelection = document.createElement("button");
+humanRockSelection.textContent = "rock";
+container.appendChild(humanRockSelection);
+const humanPaperSelection = document.createElement("button");
+humanPaperSelection.textContent = "paper";
+container.appendChild(humanPaperSelection);
+const humanScissorsSelection = document.createElement("button");
+humanScissorsSelection.textContent = "scissors";
+container.appendChild(humanScissorsSelection);
+const results = document.createElement("div");
+results.style.minHeight = "100px";
+results.style.backgroundColor = "pink";
+const resultsScoreBoard = document.createElement("div");
+resultsScoreBoard.style.backgroundColor = "lightBlue";
+resultsScoreBoard.style.minHeight = "20px";
+const resultsMessage = document.createElement("div");
+resultsMessage.style.minHeight = "20px";
+results.appendChild(resultsMessage);
+results.insertBefore(resultsScoreBoard, resultsMessage);
+container.appendChild(results);
+
+const buttons = document.querySelectorAll("button");
+// Configure value of each button to reflect text content.
+buttons.forEach((btn) => {
+  btn.value = btn.textContent;
+});
+
+container.addEventListener("click", (event) => {
+  if (event.target.value === "rock") {
+    playRound("rock");
+  } else if (event.target.value === "paper") {
+    playRound("paper");
+  } else if (event.target.value === "scissors") {
+    playRound("scissors");
+  }
+  // console.log(event.target.value);
+});
+
+let humanScore = 0;
+let computerScore = 0;
+let numRounds = 0;
+resultsScoreBoard.textContent = `
+  human score: ${humanScore}
+  computer score: ${computerScore}
+  `;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
 
@@ -6,50 +55,42 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-  let input = prompt("Please choose rock, paper, or scissors: ");
-  return "" + input.toLowerCase();
+function playRound(humChoice) {
+  // let compChoice = getComputerChoice();
+  const winningMessage = determineWinner(getComputerChoice(), humChoice);
+
+  if (winningMessage.includes("tie")) {
+  } else {
+    winningMessage.includes("Computer wins.") ? ++computerScore : ++humanScore;
+  }
+  // Update resultsMessge and resultsScoreboard
+  resultsMessage.textContent = winningMessage;
+  resultsScoreBoard.textContent = `
+  human score: ${humanScore}
+  computer score: ${computerScore}
+  `;
+  numRounds++;
+  if (numRounds >= 5) {
+    // disable clickEvent
+    // determine winner
+    // make a restart game button appear
+  }
 }
 
-function playRound(compChoice, humChoice) {
-  let winningMessage = "";
-  if (compChoice === humChoice) winningMessage = "it's a tie";
+function determineWinner(compChoice, humChoice) {
+  if (compChoice === humChoice) return "it's a tie";
   if (compChoice === "rock" && humChoice === "paper") {
-    winningMessage = "Computer chose rock & you chose paper. You win!";
+    return "Computer chose rock & you chose paper. You win!";
   } else if (compChoice === "rock" && humChoice === "scissors") {
-    winningMessage =
-      "Computer chose rock and you chose scissors. Computer wins.";
+    return "Computer chose rock and you chose scissors. Computer wins.";
   } else if (compChoice === "paper" && humChoice === "rock") {
-    winningMessage = "Computer chose paper and you chose rock. Computer wins.";
+    return "Computer chose paper and you chose rock. Computer wins.";
   } else if (compChoice === "paper" && humChoice === "scissors") {
-    winningMessage = "Computer chose paper and you chose scissors. You win!";
+    return "Computer chose paper and you chose scissors. You win!";
   } else if (compChoice === "scissors" && humChoice === "rock") {
-    winningMessage = "Computer chose scissors and you chose rock. You win!";
+    return "Computer chose scissors and you chose rock. You win!";
   } else if (compChoice === "scissors" && humChoice === "paper") {
-    winningMessage =
-      "Computer chose scissors and you chose paper. Computer wins.";
+    return "Computer chose scissors and you chose paper. Computer wins.";
   }
   return winningMessage;
 }
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-  let numRounds = 0;
-
-  while (numRounds < 5) {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-
-    let roundPlayed = playRound(computerChoice, humanChoice);
-    if (roundPlayed.includes("tie")) {
-    } else {
-      roundPlayed.includes("Computer wins.") ? ++computerScore : ++humanScore;
-    }
-    console.log(roundPlayed);
-    console.log("computer score", computerScore, "human score", humanScore);
-    numRounds++;
-  }
-}
-
-console.log(playGame());
